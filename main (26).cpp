17b2cd3546342e0190ae73435063be48b2768cd5
gargshruti <86488476+gargshruti@users.noreply.github.com>
2022-03-24 21:43:23 +0530

    Add files via upload

---
+++ main (26).cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> insertion_sort(vector<int>arr,int n){
+    int key=0,i=0,j=0;
+    for(j=1;j<n;j++){
+        key = arr[j];
+        i = j-1;
+        while( i>-1 && arr[i]>=key){
+            arr[i+1] = arr[i];
+            i = i-1;
+        }
+        arr[i+1] = key;
+    }
+    return arr;
+}
+
+
+int main()
+{
+    cout<<"Enter length of array : ";
+    int n;
+    cin>>n;
+    cout<<endl<<"Enter array elements : ";
+    vector<int>arr,ans;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    ans = insertion_sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
